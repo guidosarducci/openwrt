guidosarducci/openwrt

commit aee3594ffcb72ae3e18c3719012d52519ee2d160

verbose.mk: print ERROR messages in non-verbose
Using `make -j9` only prints a subset of messages to follow the build
process progressing. However this silently skips over errors which might
be of interested. Using `make V=s` easily floods the terminal making it
hard to find error messages between the lines.

A compromise is the usage of `$(call ERROR_MESSAGE,...)` which prints a
message in red. This function is silenced in the non-verbose mode, even
if only used at a single place in `package/Makefile` where it notifies
about a OPKG corner case.

This commit moves the `ERROR_MESSAGE` definition outside of the
`OPENWRT_VERBOSE` condition and print error messages in every mode.

With this in place further error messages are possible.

Signed-off-by: Paul Spooren <mail@aparcar.org>

include/verbose.mk
@@ -29,13 +29,13 @@ ifeq ($(IS_TTY),1)
   endif
 endif
 
+define ERROR_MESSAGE
+  printf "$(_R)%s$(_N)\n" "$(1)" >&8
+endef
+
 ifeq ($(findstring s,$(OPENWRT_VERBOSE)),)
   define MESSAGE
 	printf "$(_Y)%s$(_N)\n" "$(1)" >&8
-  endef
-
-  define ERROR_MESSAGE
-	printf "$(_R)%s$(_N)\n" "$(1)" >&8
   endef
 
   ifeq ($(QUIET),1)
@@ -60,5 +60,4 @@ else
   define MESSAGE
     printf "%s\n" "$(1)"
   endef
-  ERROR_MESSAGE=$(MESSAGE)
 endif

commit b8d63de7b77dab0b7cf1d232183160fc24f27a2c

verbose.mk: fix ERROR message in verbose mode
Fixes: aee3594ffc ("verbose.mk: print ERROR messages in non-verbose")
Signed-off-by: Tony Ambardar <itugrok@yahoo.com>

include/verbose.mk
@@ -30,13 +30,14 @@ ifeq ($(IS_TTY),1)
 endif
 
 define ERROR_MESSAGE
-  printf "$(_R)%s$(_N)\n" "$(1)" >&8
+  printf "$(_R)%s$(_N)\n" "$(1)" $(ERROR_MESSAGE_REDIR)
 endef
 
 ifeq ($(findstring s,$(OPENWRT_VERBOSE)),)
   define MESSAGE
 	printf "$(_Y)%s$(_N)\n" "$(1)" >&8
   endef
+  ERROR_MESSAGE_REDIR:=>&8
 
   ifeq ($(QUIET),1)
     ifneq ($(CURDIR),$(TOPDIR))
@@ -60,4 +61,5 @@ else
   define MESSAGE
     printf "%s\n" "$(1)"
   endef
+  ERROR_MESSAGE_REDIR:=>&2
 endif